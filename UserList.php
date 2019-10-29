d5c1d794822c08223645f412d424d750d8dff06f
김희성 <khsung0@nate.com>
2019-10-29 16:40:58 +0900

    Update UserList.php

--- UserList.php
+++ UserList.php
@@ -1,12 +1,15 @@
 <?php
-	$con = mysqli_connect("localhost", "khsung0", "gmltjd1!", "khsung0");
- 	$result = mysqli_query($con, "SELECT * FROM MEMBER");
- 	$response = array();
-
-	while($row = mysqli_fetch_array($result)){
-array_push($response, array("userID"=>$row[0], "userPassword"=>$row[1], "userName"=>$row[2], "userEmail"=>$row[3])); 
+$con = mysqli_connect('localhost', 'khsung0', 'gmltjd1!', 'khsung0');
+if(mysqli_connect_errno($con)){
+echo "Failed to connect : ".mysqli_connect_error();
 }
-
- echo json_encode(array("response"=>$response));
- mysqli_close($con);
+mysqli_set_charset($con,"utf8");
+$sql="SELECT * FROM MEMBER";
+$result = mysqli_query($con, $sql);
+$response = array();
+while($row = mysqli_fetch_array($result)){
+array_push($response, array("userID"=>$row[0], "userPassword"=>$row[1], "userName"=>$row[2], "userEmail"=>$row[3]));
+}
+echo json_encode(array("response"=>$response));
+mysqli_close($con);
 ?>
